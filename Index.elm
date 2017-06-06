4efef07bdae7109a922031eb995aae56d84492e3
Jeff Pierson <jeffrey.pierson@gmail.com>
2017-06-06 00:28:53 -0400

    Separate resume sections into functions

---
+++ Index.elm
@@ -0,0 +1,11 @@
+module Index exposing (..)
+
+import Html exposing (text, body, div, h1, h3, a, p, i, span, section, h5, br, hr, node)
+import ResumeContent exposing (resume, ResumeData)
+
+myData : ResumeData 
+myData = ResumeData "Jeff Pierson" "jeff@beyondfold.com"
+
+main : Html.Html msg
+main = resume myData
+   
