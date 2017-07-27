5828d5ff6f7a50d6f729d27bd6c78d9e042aa79a
Jeff Pierson <jeffrey.pierson@gmail.com>
2017-07-26 21:26:41 -0400

    Add documentation and comments

--- Html/Knommon.elm
+++ Html/Knommon.elm
@@ -2,6 +2,10 @@ module Html.Knommon exposing (..)
 
 import Html exposing (node)
 
+-- Not sure if these element types are valid or what they do
+-- but they are used within the original example from knommon 
+-- see https://github.com/knommon/resume/blob/6179c9dab05b292c4a81e157db27f6f22ad63768/index.html
+
 sm : List (Html.Attribute msg) -> List (Html.Html msg) -> Html.Html msg
 sm =
     node "sm"
